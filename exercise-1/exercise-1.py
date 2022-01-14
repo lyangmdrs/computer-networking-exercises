865c5cbbf29257f23943970ee92a2543ada09b87
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-01-14 15:35:21 -0300

    Shows the received message

--- exercise-1/exercise-1.py
+++ exercise-1/exercise-1.py
@@ -3,6 +3,7 @@ import socket
 IP_ADDRS = '127.0.0.1'
 PORT_NUM = 50500
 FULL_ADDRS = (IP_ADDRS, PORT_NUM)
+PACKET_SIZE = 1024
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
@@ -13,6 +14,10 @@ server_sock.listen(1)
 
 print('Waiting for client...')
 
-client_sock, client_addr = server_sock.accept()
+connectionSocket, client_addr = server_sock.accept()
 
 print('Client connected from', client_addr)
+
+message = connectionSocket.recv(PACKET_SIZE)
+
+print('Request received:', message.decode(), sep='\n\n')
