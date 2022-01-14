4e5059bd01ac1707133144faf0607ce5890f532b
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-01-14 20:16:33 -0300

    Responding with a complete HTML page

--- exercise-1/exercise-1.py
+++ exercise-1/exercise-1.py
@@ -4,10 +4,13 @@ IP_ADDRS = '127.0.0.1'
 PORT_NUM = 50500
 FULL_ADDRS = (IP_ADDRS, PORT_NUM)
 PACKET_SIZE = 1024
+OBJ_TAG = '<object data="{}"></object>'
+MSG_TAG = '<h3>{} - {}</h3>'
+HTML_PAGE = '<html><head><h1>Exercise 1</h1></head><body><h3>{} - {}</h3><object data="{}"></object></body></html>'
 
 def messageParser(message):
     ''''''
-    lines = message.split('\n')
+    lines = message.splitlines()
     
     request_line = lines[0]
     [method, url, version] = request_line.split(' ')
@@ -17,15 +20,28 @@ def messageParser(message):
     print('Version:', version)
     
     if method != 'GET':
-        return (501, 'Not Implemented', version)
+        return ('501', 'Not Implemented', version, '')
     
-    return(200, 'OK', version)
+    if url == '/':
+        print('Root path request!')
+        return('200', 'OK', version, '')
 
-def getHtmlContent():
-    html_content = ''
-    with open('responseMessage.html') as response_message_file:
-        html_content = response_message_file.read()
-    return html_content
+    file_name = url.replace('/', '')
+    print('Requesting the file:', file_name)
+
+    try:
+        with open(file_name) as f:
+            f.close()
+            pass
+
+    except IOError:
+        print('The file "{}" was not found!'.format(file_name))
+        return('404', 'Not Found', version, '')
+
+    else:
+        print('The file "{}" is present!'.format(file_name))
+        return('200', 'OK', version, file_name)
+
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
@@ -42,14 +58,13 @@ print('Client connected from', client_addr)
 
 message = connection_socket.recv(PACKET_SIZE)
 
-print('Request received:', message.decode(), sep='\n\n')
-
-(status_code, status_msg, version) = messageParser(message.decode())
+(status_code, status_msg, version, file_name) = messageParser(message.decode())
 
 status_line = '{} {} {}\n'.format(version, status_code, status_msg)
 header_line = ''
 blank_line = '\n'
-body = getHtmlContent()
+
+body = HTML_PAGE.format(status_code, status_msg, file_name)
 
 response_message = status_line + header_line + blank_line + body
 
