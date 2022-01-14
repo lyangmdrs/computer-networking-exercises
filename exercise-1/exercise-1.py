bc8f01ab21829a1cbdb1814a9b160a4d4e32d94c
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-01-14 16:27:47 -0300

    Parsing the request line

--- exercise-1/exercise-1.py
+++ exercise-1/exercise-1.py
@@ -1,10 +1,22 @@
-from http.client import HTTPResponse
 import socket
 
 IP_ADDRS = '127.0.0.1'
 PORT_NUM = 50500
 FULL_ADDRS = (IP_ADDRS, PORT_NUM)
 PACKET_SIZE = 1024
+
+def messageParser(message):
+    ''''''
+    decoded_msg = message.decode()
+    lines = decoded_msg.split('\n')
+    
+    request_line = lines[0]
+    [method, url, version] = request_line.split(' ')
+
+    print('Method:', method)
+    print('URL:', url)
+    print('Version:', version)
+    
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 
@@ -20,7 +32,8 @@ connection_socket, client_addr = server_sock.accept()
 print('Client connected from', client_addr)
 
 message = connection_socket.recv(PACKET_SIZE)
+messageParser(message)
 
-print('Request received:', message.decode(), sep='\n\n')
+#print('Request received:', message.decode(), sep='\n\n')
 
-connection_socket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+connection_socket.send('HTTP/1.0 200 OK\r\n\r\n<html></html><head></head><body>OK 200</body>'.encode())
