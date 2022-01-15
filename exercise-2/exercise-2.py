567238e60f72a851309fc1de65ec57196d7f6bf5
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-01-15 16:01:19 -0300

    Treating commands with handler functions

--- exercise-2/exercise-2.py
+++ exercise-2/exercise-2.py
@@ -5,11 +5,36 @@ PORT_NUM = 2121
 FULL_ADDRS = (IP_ADDRS, PORT_NUM)
 PACKET_SIZE = 1024
 
-FTP_COMMANDS = {'OPTS': b'202 Command not implemented.\r\n', 
-                'USER': b'331 User name okay, need password.\r\n',
-                'PASS': b'230 Login Successful.\r\n',
-                'PORT': b'200 Connected.\r\n',
-                'QUIT': b'200 Goodbye.\r\n',
+def client_connected_handler():
+    return '220 Service ready.\r\n'
+
+def command_parser(message):
+    
+    message_parts = message.split(' ')
+    ftp_command = message_parts[0].strip()
+    return ftp_command
+
+def opts_command_handler():
+    return '202 Command not implemented.\r\n'
+
+def user_command_handler():
+    return '331 User name okay, need password.\r\n'
+
+def pass_command_handler():
+    return '230 Login Successful.\r\n'
+    
+def port_command_handler():
+    return '200 Connected.\r\n'
+    
+def quit_command_handler():
+    return '200 Goodbye.\r\n'
+    
+
+FTP_COMMANDS = {'OPTS': opts_command_handler, 
+                'USER': user_command_handler,
+                'PASS': pass_command_handler,
+                'PORT': port_command_handler,
+                'QUIT': quit_command_handler,
                  }
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -25,13 +50,18 @@ connection_socket, client_addr = server_sock.accept()
 
 print('Client connected from', client_addr)
 
-connection_socket.send('220 Service ready.\r\n'.encode())
+connection_socket.send(client_connected_handler().encode())
 
-message = connection_socket.recv(PACKET_SIZE)
-print('Received Message:', message)
+function_handler = None
 
-connection_socket.send('202 UTF8 enabled.\r\n'.encode())
+while function_handler != FTP_COMMANDS['QUIT']:
+    message = connection_socket.recv(PACKET_SIZE)
+    print('Received Message:', message)
+    function_handler = FTP_COMMANDS[command_parser(message.decode())]
+    response = function_handler()
+    connection_socket.send(response.encode())
 
+'''
 message = connection_socket.recv(PACKET_SIZE)
 print('Received Message:', message)
 
@@ -44,5 +74,6 @@ connection_socket.send('230 Login Successful.\r\n'.encode())
 
 message = connection_socket.recv(PACKET_SIZE)
 print('Received Message:', message)
+'''
 
 connection_socket.close()
